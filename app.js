1e28c97da56197e7e35d84458348ad6dbccc006c
KALEEMULLAH <160345037+Kaleemullah3095@users.noreply.github.com>
2024-03-06 23:54:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var Members = ["kaleemullah", "karamatullah", "zikrullah", "karamullah"];
+for (var i = 0; i < Members.length; i++) {
+    console.log("Hello, ".concat(Members[i], "!Have a great day!"));
+}
---
+++ app.ts
@@ -0,0 +1,4 @@
+let Members:string[]=["kaleemullah","karamatullah","zikrullah","karamullah"];
+for (let i=0;i <Members.length; i++) {
+    console.log(`Hello, ${Members[i]}!Have a great day!`);
+}
